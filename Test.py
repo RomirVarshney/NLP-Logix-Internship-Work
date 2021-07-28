363b3f9e341f0fb6384f138da39e91bf13dcf682
RomirVarshney <romir.varshney@gmail.com>
2021-07-27 20:57:33 -0400

    Finalized Python Model
    Finalized the code for the machine learning model.

--- Test.py
+++ Test.py
@@ -13,6 +13,7 @@ nfl = pd.read_csv("NFL Play by Play 2009-2018 (v5).csv", low_memory=False)
  #   print(col_name)
 #print("\n")
 
+# Converting Categorical Play Type Values to Numeric
 def isPass(nfl):
     if(nfl["play_type"]) == "pass":
         return 1
@@ -23,11 +24,12 @@ def isPass(nfl):
 
 
 
-
+# Creating y-variable
 nfl["yvar"] = nfl.apply(isPass, axis=1)
 nfl= nfl[nfl["yvar"].notna()]
 #print(nfl["yvar"].dtype)
 
+# Combining Posteam Variables to Correct Values
 nfl["posteam"].replace("JAC", "JAX", inplace =True)
 nfl["posteam"].replace("STL", "LA", inplace =True)
 nfl["posteam"].replace("SD", "LAC", inplace =True)
@@ -48,21 +50,44 @@ nfl["posteam"].replace("SD", "LAC", inplace =True)
 #droppedy= droppedy.dropna()
 #nfl.head()
 
-nfl["ratio"]= nfl['score_differential'] / nfl['game_seconds_remaining'] 
+# Creating the Ratio Features with Score Differential and Game Seconds
+nfl["ratio"] = nfl['score_differential'] / nfl['game_seconds_remaining']
 
-
-features= ["yardline_100", "score_differential", "ydstogo", "game_seconds_remaining", "no_huddle", "down", "half_seconds_remaining", "quarter_seconds_remaining", "goal_to_go", "posteam_score", "defteam_score", "shotgun", "goal_to_go" , "posteam_timeouts_remaining", "defteam_timeouts_remaining", "total_home_score", "total_away_score"]
-newFeatures = [
-    "avg_pass_rate",
-    'ratio'
+# All Original Features
+features = [
+"yardline_100",
+"score_differential",
+"ydstogo",
+"game_seconds_remaining",
+"no_huddle",
+"down",
+"half_seconds_remaining",
+"quarter_seconds_remaining",
+"posteam_score",
+"defteam_score",
+"shotgun",
+"goal_to_go",
+"posteam_timeouts_remaining",
+"defteam_timeouts_remaining",
+"total_home_score",
+"total_away_score"
 ]
 
+# All Newly Created Features
+newFeatures = [
+"avg_pass_rate",
+'ratio',
+]
+
+# Filling in Feature Null Values with -999
 nfl.replace([np.inf, -np.inf], np.nan, inplace = True)
 nfl[features]= nfl[features].fillna(-999)
 
 
-print(nfl.loc[nfl['game_seconds_remaining']== 0])
 
+#print(nfl.loc[nfl['game_seconds_remaining']== 0])
+
+# Seperating Data (Pass Percentage) by Posteam and Year
 nfl["year"] = pd.to_datetime(nfl["game_date"], yearfirst=True, errors='coerce')
 nfl["year"] = nfl["year"].dt.year
 nfl_team_yards = nfl.groupby(["posteam", "year"])["yvar"].mean()
@@ -70,38 +95,51 @@ nfl_team_yards = nfl.groupby(["posteam", "year"])["yvar"].mean()
 nfl = pd.merge(nfl, nfl_team_yards, how="left", on=["posteam", "year"])
 nfl.rename(columns={"yvar_y": "avg_pass_rate"}, inplace=True)
 
+# Dropping Ratio Null Values (Dividing by Zero)
+nfl["ratio"]= nfl["ratio"].fillna(-999)
+
 #playtime = 'score_differential'/ 'game_seconds_remaining'
 #fillFeatures= pd.DataFrame(nfl[features])
 #fillFeatures= fillFeatures.dropna()
 
+# Adding y-variable to Data Set
 nfl["yvar"] = nfl.apply(isPass, axis=1)
 
+# Combining Features
 features = features + newFeatures
 
-x= nfl[features]
-y= nfl["yvar"]
+# Creating X and Y Variables
+x = nfl[features]
+y = nfl["yvar"]
 
 #print(nfl[features])
 #print(nfl[features].dtypes)
 #print(nfl[features].isnull().sum())
 #print(x.shape)
 
+# Creating the Train/Test Split, and Random Forest Classifier
 train_X, test_X, train_y, test_y = train_test_split(x, y, train_size=0.7, test_size=0.3, random_state=1)
 model = RandomForestClassifier(n_estimators=175, max_depth=13, random_state=1)
 model.fit(train_X,train_y)
 predictions = model.predict_proba(test_X)
 
+# Printing ROC AUC Score
+print()
 result = roc_auc_score(test_y,predictions[:, 1])
 print("ROC AUC Score: " + str(result))
+print()
 
+# Creating, Sorting, and Printing Feature Importance
 feature_importance = model.feature_importances_
 feature_importance_df = pd.DataFrame(feature_importance, index=train_X.columns, columns=["Rate of Importance"])
-feature_importance_df.sort_values("Rate of Importance")
-print(feature_importance_df)
+print(feature_importance_df.sort_values("Rate of Importance", ascending = False))
+print()
+#print(feature_importance_df.sort_values)
 
 #accuracyScore= (metrics.accuracy_score(test_y, predictions))
 #print(accuracyScore)
 
+# Outputting Predictions and Actual Play to CSV File
 output = pd.DataFrame({'Predicted_play':predictions[:, 1], 'Actual_Play': test_y})
 output.to_csv("predictions1.csv")
 #print(predictions)
